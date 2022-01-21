50f75199ebdc34ba85d620ab0dca64991eeca0a2
Drew Cook <drewcook29@gmail.com>
2022-01-20 17:22:42 -0700

    feat: Set up Twilio SMS service

---
+++ secrets.example.js
@@ -0,0 +1,14 @@
+/**
+ * https://www.twilio.com/docs/usage/tutorials/how-to-use-your-free-trial-account
+ * 1. Register an account with Twilio - https://twilio.com
+ * 2. Register a phone number
+ * 3. Set up an SMS messaging service in your account dashboard
+ * 4. Tie the number with the service
+ * 5. Fill in the valuus below with your Twilio information
+ */
+
+module.exports = {
+	TWILIO_ACCOUNT_SID: 'your-account-sid',
+	TWILIO_AUTH_TOKEN: 'your-auth-token',
+	TWILIO_PHONE: '+15555555555',
+}
